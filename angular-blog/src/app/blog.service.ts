1c3278a0b4194de0b553637a31173ad287e79d1e
TommyW9 <yw1607@nyu.edu>
2018-11-17 20:05:18 -0800

    part d

--- angular-blog/src/app/blog.service.ts
+++ angular-blog/src/app/blog.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 @Injectable({
   providedIn: 'root'
 })
@@ -10,9 +15,53 @@ export class Post {
   modified: Date;
   title: string;
   body: string;
+
+  constructor(postid, created, modified, title, body){
+    this.postid = postid;
+    this.created = created;
+    this.modified = modified;
+    this.title = title;
+    this.body = body;
+  }
 }
 
 export class BlogService {
+  private baseUrl = 'api';  // URL to web api
+  private http: HttpClient;
   private posts: Post[];
   constructor() { }
+
+  fetchPosts (username : string): void{
+    this.http.get(this.baseUrl);
+  }
+
+  getPosts(username: string): Post[]{
+    return this.posts;
+  }
+
+  getPost(username: string, id: number): Post{
+    return this.posts.filter(post => post.postid === id)[0];
+  }
+
+  newPost(username: string): Post{
+    const time = new Date();
+    let postid = 0;
+    const url = '${baseUrl}/${username}/${postid}';
+    let post = new Post(postid, time, time, '','');
+    this.http.post(url, post, httpOptions);
+    this.posts.push(post);
+    // add response handler
+    return post;
+  }
+
+  updatePost(username: string, post: Post): void{
+    let p = this.posts.filter(p => p.postid == post.postid).forEach(p => {
+      p.title = post.title;
+      p.body = post.body;
+    });
+    const url = '${baseUrl}/${username}/${postid}';
+    this.http.put(url, p, httpOptions);
+  }
 }
+
+
