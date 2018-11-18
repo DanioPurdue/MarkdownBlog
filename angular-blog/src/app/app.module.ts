d7a31bc06cc8fff0f28998cc48fad56308fae0b3
TommyW9 <yw1607@nyu.edu>
2018-11-17 23:55:45 -0800

    f

--- angular-blog/src/app/app.module.ts
+++ angular-blog/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { EditComponent } from './edit/edit.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
